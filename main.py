4115798d1b69a108a9cfa9fbc812fb3de6f08b7e
Muhammad Ahmad Amin <ahmadaminmuhammad1@gmail.com>
2025-12-04 00:05:21 +0500

    Server Management

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import threading
 from gui.app import SecureVPNApp
+from start_servers import start_all_servers
 
 if __name__ == "__main__":
+    # Run servers in background
+    threading.Thread(target=start_all_servers, daemon=True).start()
+
+    # Start GUI
     app = SecureVPNApp()
     app.mainloop()
